706a3dc02fe3893e34e2357e731ac62d091f4ed3
PhilipZhangs <zhangsphilip@gmail.com>
2024-02-14 07:09:01 -0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by 张胖子 on 2024-02-14.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by 张胖子 on 2024-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
