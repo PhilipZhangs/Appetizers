c9892816f150b42a5f8f5d0654ded9a69c2c2360
PhilipZhangs <zhangsphilip@gmail.com>
2024-03-05 00:55:46 -0800

    added extension for color and uicolor, added loadingview

---
+++ Appetizers/Color+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by 张胖子 on 2024-03-05.
+//
+
+import SwiftUI
+
+//extension Color {
+//    static let brandPrimary = Color("brandPrimary")
+//}
+//
+//
+//extension UIColor {
+//    static let brandPrimary = UIColor(named: "brandPrimary")
+//}
